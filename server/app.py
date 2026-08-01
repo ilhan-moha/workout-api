e89da0a9aebc42deac07c5947575045482205227
ilhan moha <mohamudilhan99@gmail.com>
2026-08-01 17:42:55 +0300

    Set up Flask application

---
+++ server/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_migrate import Migrate
+
+from models import db
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+#routes
+
+@app.route("/")
+def home():
+    return {
+        "message": "Welcome to the Workout Tracker API!"
+    }, 200
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
